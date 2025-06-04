6c7216a9261dd541c773fc6f742672ec58c73d86
nahidhk <nahidappltd@gmail.com>
2025-06-05 01:56:30 +0600

    new update

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+console.log("main.js loaded");
+
+function closepopup(loaddata){
+    document.getElementById(loaddata).style.display = "none";
+}
+function openpopup(loaddata){
+    document.getElementById(loaddata).style.display = "flex";
+}
