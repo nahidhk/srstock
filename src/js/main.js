8e811ca98f923e0fab9535137f317ba2abe5fa19
nahidhk <nahidappltd@gmail.com>
2025-06-07 21:09:26 +0600

    new update

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,39 @@
 
 console.log("main.js loaded");
 
-function closepopup(loaddata){
+function closepopup(loaddata) {
     document.getElementById(loaddata).style.display = "none";
 }
-function openpopup(loaddata){
+function openpopup(loaddata) {
     document.getElementById(loaddata).style.display = "flex";
 }
+
+
+
+
+// Tools function of backend by Nahid TDX 
+
+fetch('/tools/data/srs_pp_code.json')
+    .then(response => response.json())
+    .then(data => {
+        const carasoleData = `
+
+    <div class="padding flex center wrap row mw30">
+                <div class="underline">${data.title}</div>
+                <p class=" summary">${data.summary}</p>
+               <div class="text-center">
+                <button onclick="location.href='${data.link}'" class="btn black-btn cont">
+                    ${data.btnName} &nbsp;
+                    <i class="fa-solid fa-arrow-right"></i>
+                </button>
+                </div>
+     </div>
+
+            <div class="flex medel around mw50">
+                <img class="feetImg" src="/src/img/${data.image}" alt="${data.imageAlt}" srcset="">
+            </div>
+
+   `
+        document.getElementById("story").innerHTML = carasoleData;
+    })
+    .catch(error => console.error("Error loading data:", error));
